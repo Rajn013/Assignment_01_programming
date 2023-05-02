c26bcbfcb0aa5e488475e383bbf31b06b55f252d
Rajndep singh matharu <105154536+Rajn013@users.noreply.github.com>
2023-05-02 15:17:55 +0530

    Add files via upload

---
+++ Untitled33.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.Write a Python program to print "Hello Python"?
+
+# In[1]:
+
+
+print("hello python")
+
+
+# In[ ]:
+
+
+2.Write a Python program to do arithmetical operations addition and division.?
+
+
+# In[2]:
+
+
+num1 = 19
+num2 = 98
+
+sum = num1 + num2
+division = num1 / num2
+
+
+print("The sum of", num1, "and", num2, "is", sum)
+print("The division of", num1, "and", num2, "is", division)
+
+
+# 3.Write a Python program to find the area of a triangle?
+
+# In[3]:
+
+
+base = 9
+height = 8
+area = 0.5 * base * height
+print("The area of the triangle is", area)
+
+
+# 4.Write a Python program to swap two variables?
+
+# In[4]:
+
+
+x = 15
+y = 20
+print("Before swapping: x =", x, "and y =", y)
+x, y = y, x
+print("After swapping: x =", x, "and y =", y)
+
+
+# 5.Write a Python program to generate a random number?
+
+# In[5]:
+
+
+import random
+random_number = random.randint(1, 100)
+print("The random number is:", random_number)
+
+
+# In[ ]:
+
+
+
+
